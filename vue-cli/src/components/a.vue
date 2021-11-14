e3cb24b950b821236ea6bca4c524267838e67ecd
Minju Ko <70134713+Jerry-ko@users.noreply.github.com>
2021-11-14 18:06:38 +0900

    싱글 파일 컴포넌트 소개 및 여태까지 배운 내용 적용하는 방법

---
+++ vue-cli/src/components/a.vue
@@ -0,0 +1,20 @@
+var appHeader = { template: '
+<div>header</div>
+', method: { addNum: function() { } } }
+
+<template>
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  //  Javascript - 인스턴스 옵션
+  method: {
+    addNum: function () {},
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+/* css*/
+</style>
